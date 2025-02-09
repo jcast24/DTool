ee789be5e8e7f850c97fdcf1450c043b3c92d62b
Jesus Castro <castrojesus39@gmail.com>
2025-02-08 23:27:39 -0800

    created ShowDrives() method

--- DTool/Form1.cs
+++ DTool/Form1.cs
@@ -2,8 +2,31 @@ namespace DTool;
 
 public partial class Form1 : Form
 {
+    public void ShowDrives()
+    {
+        treeView1.BeginUpdate();
+        string[] drives = Directory.GetLogicalDrives();
+        foreach (string adrive in drives)
+        {
+            TreeNode tn = new TreeNode(adrive);
+            treeView1.Nodes.Add(tn);
+        }
+        
+        treeView1.EndUpdate();
+    }
+    
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
     }
 }
